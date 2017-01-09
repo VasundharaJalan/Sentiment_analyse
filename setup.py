ddd8f5cf6af09afc6bbdcb10422303eb377f2511
VasundharaJalan <jalanvasundhara.aiesec@gmail.com>
2017-01-09 12:59:18 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sentiment_analyse',
+  packages = ['sentiment_analyse'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Vasundhara Jalan',
+  author_email = 'jalanvasundhara.aiesec@gmail.com',
+  url = 'https://github.com/VasundharaJalan/Sentiment_analyse/tree/master/sentiment_analyse/run', # use the URL to the github repo
+  download_url = 'https://github.com/VasundharaJalan/Sentiment_analyse/tree/0.1', # I'll explain this in a second
+  keywords = ['testing', 'sentiment', 'sentence'], # arbitrary keywords
+  classifiers = [],
+)
