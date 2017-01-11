809b62d998d74b65646e712861ecfe46e77179da
VasundharaJalan <jalanvasundhara.aiesec@gmail.com>
2017-01-11 13:44:33 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'sentiment_analyse',
+    packages = ['sentiment_analyse'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'analysis of sentiments of sentences',
+    author = 'Vasundhara Jalan',
+    author_email = 'vasudplayer@gmail.com',
+    url = 'https://github.com/VasundharaJalan/Sentiment_analyse',
+    download_url = 'https://github.com/VasundharaJalan/Sentiment_analyse/releases/tag/0.1',
+    keywords = ['0.1'],
+    classifiers = [],
+)
